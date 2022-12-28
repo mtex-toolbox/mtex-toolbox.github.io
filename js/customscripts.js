6052539dabd188c8d40bdf6be5c78b842e65b386
FabianBartl <fabian@informatic-freak.de>
2022-12-28 15:30:22 +0100

    fix: hover check

--- js/customscripts.js
+++ js/customscripts.js
@@ -63,7 +63,7 @@ $(document).ready(function() {
             $(this).html(unfolded);
             $(this).attr("unfolded", "true");
         } else {
-            if ($(this).children('span')[0].is(':hover')) {
+            if (!$(this).children('div').is(':hover')) {
                 $(this).html(folded);
                 $(this).attr("unfolded", "false");
             }
