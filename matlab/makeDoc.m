ae52fc409c61c68e30008fffcfc53a6c6585e475
Ralf Hielscher <ralfHielscher@users.noreply.github.com>
2024-11-07 10:25:46 +0100

    work on

--- matlab/makeDoc.m
+++ matlab/makeDoc.m
@@ -41,77 +41,88 @@ options.tmpDir = fullfile(pwd,'tmp');
 options.imageDir = fullfile(pwd,'..','images');
 options.LaTex = 'mathJax';
 options.publishSettings.stylesheet = fullfile(pwd,'web.xsl');
+options.force = check_option(varargin,'force');
 
 options.xml.toolbox.versionName.Text = getMTEXpref('version');
 options.xml.toolbox.name.Text = 'MTEX';
 options.xml.toolbox.fullname.Text = '<b>MTEX</b> - A MATLAB Toolbox for Quantitative Texture Analysis';
 options.xml.toolbox.lastUpdated.Text = date;
 
+doAll = ~check_option(varargin,{'ref','doc','examples'});
+
 %% Publish Function Reference
 
-% define source files
-mtexFunctionFiles = [...
-  DocFile( fullfile(mtex_path,'S2Fun')) ...
-  DocFile( fullfile(mtex_path,'SO3Fun')) ...
-  DocFile( fullfile(mtex_path,'EBSDAnalysis')) ...
-  DocFile( fullfile(mtex_path,'ODFAnalysis')) ...
-  DocFile( fullfile(mtex_path,'PoleFigureAnalysis')) ...
-  DocFile( fullfile(mtex_path,'TensorAnalysis')) ...
-  DocFile( fullfile(mtex_path,'plotting')) ...
-  DocFile( fullfile(mtex_path,'geometry')) ...
-  DocFile( fullfile(mtex_path,'interfaces')) ...
-  DocFile( fullfile(mtex_path,'tools')) ...
-  DocFile( fullfile(mtex_path,'doc','FunctionReference'));];
+if doAll || check_option(varargin,'ref')
 
-% make sidebar
-makeHelpToc(mtexFunctionFiles,'FunctionReference','funcRef.xml');
-xml2yml('funcRef.xml','../_data/sidebars/function_reference_sidebar.yml','Functions')
+  % define source files
+  mtexFunctionFiles = [...
+    DocFile( fullfile(mtex_path,'S2Fun')) ...
+    DocFile( fullfile(mtex_path,'SO3Fun')) ...
+    DocFile( fullfile(mtex_path,'EBSDAnalysis')) ...
+    DocFile( fullfile(mtex_path,'ODFAnalysis')) ...
+    DocFile( fullfile(mtex_path,'PoleFigureAnalysis')) ...
+    DocFile( fullfile(mtex_path,'TensorAnalysis')) ...
+    DocFile( fullfile(mtex_path,'plotting')) ...
+    DocFile( fullfile(mtex_path,'geometry')) ...
+    DocFile( fullfile(mtex_path,'interfaces')) ...
+    DocFile( fullfile(mtex_path,'tools')) ...
+    DocFile( fullfile(mtex_path,'doc','FunctionReference'));];
 
-% publish files
-funOut = fullfile(pwd,'..','pages','function_reference_matlab');
-options.outDir = funOut;
-options.xml.toolbox.folder.Text = 'function_reference';
+  % make sidebar
+  makeHelpToc(mtexFunctionFiles,'FunctionReference','funcRef.xml');
+  xml2yml('funcRef.xml','../_data/sidebars/function_reference_sidebar.yml','Functions')
 
-%options.force = true;
-publish(mtexFunctionFiles,options);
-%options.force = false;
+  % publish files
+  funOut = fullfile(pwd,'..','pages','function_reference_matlab');
+  options.outDir = funOut;
+  options.xml.toolbox.folder.Text = 'function_reference';
+
+  options.force = check_option(varargin,'force');
+  publish(mtexFunctionFiles,options);
+
+end
 
 %% Publish Doc
 
-% define source files
-mtexDocFiles = DocFile( fullfile(mtex_path,'doc'));
-mtexDocFiles = exclude(mtexDocFiles,'makeDoc','html','FunctionReference');
+if doAll || check_option(varargin,'doc')
 
-% make sidebar
-makeHelpToc(mtexDocFiles,'Documentation','doc.xml');
-xml2yml('doc.xml','../_data/sidebars/documentation_sidebar.yml','Topics')
+  % define source files
+  mtexDocFiles = DocFile( fullfile(mtex_path,'doc'));
+  mtexDocFiles = exclude(mtexDocFiles,'makeDoc','html','FunctionReference');
 
-% publsih files
-docOut = fullfile(pwd,'..','pages','documentation_matlab');
-options.outDir = docOut;
-options.xml.toolbox.folder.Text = 'documentation';
+  % make sidebar
+  makeHelpToc(mtexDocFiles,'Documentation','doc.xml');
+  xml2yml('doc.xml','../_data/sidebars/documentation_sidebar.yml','Topics')
 
-publish(mtexDocFiles,options);
+  % publish files
+  docOut = fullfile(pwd,'..','pages','documentation_matlab');
+  options.outDir = docOut;
+  options.xml.toolbox.folder.Text = 'documentation';
 
+  publish(mtexDocFiles,options);
+end
 
 %% make examples
 
-% define source files
-mtexExFiles = DocFile( fullfile(mtex_path,'..','examples'));
-mtexExFiles = exclude(mtexExFiles,'JAC-Creuziger');
+if doAll || check_option(varargin,'examples')
+
+  % define source files
+  mtexExFiles = DocFile( fullfile(mtex_path,'..','examples'));
+  mtexExFiles = exclude(mtexExFiles,'JAC-Creuziger');
 
 
-% make sidebar
-makeHelpToc(mtexExFiles,'Examples','examples.xml');
-xml2yml('examples.xml','../_data/sidebars/examples_sidebar.yml','Examples')
+  % make sidebar
+  makeHelpToc(mtexExFiles,'Examples','examples.xml');
+  xml2yml('examples.xml','../_data/sidebars/examples_sidebar.yml','Examples')
 
-% publish files
-exOut = fullfile(pwd,'..','pages','examples_matlab');
-options.outDir = exOut;
-options.xml.toolbox.folder.Text = 'examples';
-options.publishSettings.stylesheet = fullfile(pwd,'examples.xsl');
+  % publish files
+  exOut = fullfile(pwd,'..','pages','examples_matlab');
+  options.outDir = exOut;
+  options.xml.toolbox.folder.Text = 'examples';
+  options.publishSettings.stylesheet = fullfile(pwd,'examples.xsl');
 
-publish(mtexExFiles,options);
+  publish(mtexExFiles,options);
+end
 
 %% check links
 
